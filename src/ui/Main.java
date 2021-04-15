f7fbe18b6448daf239b576de8a4c7146d0743b49
AngelicaCorrales <angelicacorrales2002@gmail.com>
2021-04-15 08:07:06 -0500

    cycle added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -21,36 +21,48 @@ public class Main {
 	}
 	public static void main(String[] args) throws IOException {
 		Main m= new Main();
-		/*int line= Integer.parseInt(m.br.readLine());
+		int line= Integer.parseInt(m.br.readLine());
+		
+		
+		m.cycle(line);
 
-		if(line==1) {
-
-		}else {
-			line--;
-		}*/
-
-		int numb= Integer.parseInt(m.br.readLine());
-		m.a[1]= numb;
-		m.bw.write(m.a[1]+""+m.b[1]+""+m.c[1]+"\n");
-
-		m.hanoi(numb, m.a, m.c, m.b);
+		
 		m.br.close();
 		m.bw.close();
 	}
 
+	public void cycle(int line) throws IOException {
+		if(line==0) {
+			return;
+		}else {
+			int numb= Integer.parseInt(br.readLine());
+			a[1]= numb;
+			bw.write(a[1]+""+b[1]+""+c[1]+"\n");
 
+			hanoi(numb, a, c, b);
+			bw.write("\n");
+			a[1]=0;
+			b[1]=0;
+			c[1]=0;
+
+			line--;
+			cycle(line);
+		}
+	}
+	
 	public void hanoi(int n, int[] from, int[] to, int[] aux) throws IOException {
 		if(n==1) {
 			from[1]--;
 			to[1]++;
 			bw.write(a[1]+""+b[1]+""+c[1]+"\n");
 			return;
+		}else {
+			hanoi(n-1,from,aux, to);
+			from[1]--;
+			to[1]++;
+			bw.write(a[1]+""+b[1]+""+c[1]+"\n");
+			hanoi(n-1, aux, to, from);
 		}
-		hanoi(n-1,from,aux, to);
-		from[1]--;
-		to[1]++;
-		bw.write(a[1]+""+b[1]+""+c[1]+"\n");
-		hanoi(n-1, aux, to, from);
 
 	}
 
